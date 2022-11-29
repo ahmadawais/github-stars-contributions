ea96e1c3117f329240a15e3d6afb4e0d64005f71
Ahmad Awais <me@AhmadAwais.com>
2022-11-28 19:16:53 -0800

    📦 NEW: API Endpoint

--- utils/auth.js
+++ utils/auth.js
@@ -4,7 +4,7 @@ const { GraphQLClient } = require('graphql-request');
 module.exports = async () => {
 	const token = await fetchToken();
 
-	const endpoint = 'https://github-stars-api.herokuapp.com/';
+	const endpoint = 'https://api-stars.github.com/';
 	const client = new GraphQLClient(endpoint, {
 		headers: { authorization: `Bearer ${token}` }
 	});
